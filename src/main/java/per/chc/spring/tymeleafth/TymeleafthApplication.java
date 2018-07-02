8250708200839214caa9423f0d545eb62501baf4
carlos <hdzcarlos>
2018-07-02 22:27:08 +0200

    Initial commit

---
+++ src/main/java/per/chc/spring/tymeleafth/TymeleafthApplication.java
@@ -0,0 +1,12 @@
+package per.chc.spring.tymeleafth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymeleafthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TymeleafthApplication.class, args);
+	}
+}
